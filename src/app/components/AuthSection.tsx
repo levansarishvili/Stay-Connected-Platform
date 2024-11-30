0685612189ff428918577a431f32524a4cdc50b3
Guranda Lemonjava <109870250+guranda26@users.noreply.github.com>
2024-11-30 17:36:20 +0400

    change font size of the login/register

--- src/app/components/AuthSection.tsx
+++ src/app/components/AuthSection.tsx
@@ -10,8 +10,8 @@ const AuthSection = ({
 }) => {
   return (
     <section className="flex justify-center items-center min-h-screen bg-gray-100 dark:bg-gray-900 mx-auto">
-      <form className="w-[500px] h-auto p-5 sm:p-10 border border-gray-300 dark:border-gray-700 bg-white dark:bg-gray-800 rounded-lg shadow-lg space-y-6 text-center">
-        <h2 className="text-3xl sm:text-4xl font-bold text-gray-800 dark:mainText">
+      <form className="w-[500px] h-auto p-8 sm:p-16 border border-gray-300 dark:border-gray-700 bg-white dark:bg-gray-800 rounded-lg shadow-lg space-y-10 text-center">
+        <h2 className="text-5xl sm:text-6xl font-bold text-gray-800 dark:mainText">
           {authType}
         </h2>
         {children}
--- src/app/components/Input.tsx
+++ src/app/components/Input.tsx
@@ -11,7 +11,7 @@ const InputComponent = ({
     <div className="flex justify-center">
       <Input
         type={inputType}
-        className="border border-gray-300 dark:border-gray-600 w-full max-w-[500px] p-5 rounded-md text-gray-800 dark:text-gray-200 dark:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500 my-1"
+        className="border border-gray-300 dark:border-gray-600 w-full max-w-[500px] p-8 rounded-md text-gray-800 dark:text-gray-200 dark:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500 my-2"
         placeholder={placeholder}
       />
     </div>
