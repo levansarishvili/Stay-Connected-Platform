1a22c65ff3509a3701af50276ada5857857d586d
levani_22 <97299346+levansarishvili@users.noreply.github.com>
2024-12-02 00:08:44 +0400

    feat: add loading spinner

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,20 @@
 import Image from "next/image";
 import { Avatar, AvatarFallback, AvatarImage } from "../components/ui/avatar";
+import Link from "next/link";
 
 export default function Header() {
   return (
     <header className="bg-primary">
       <div className="flex gap-10 items-center justify-between max-w-[136rem] px-8 py-6 mx-auto">
-        <Image
-          src="./assets/logo.svg"
-          alt="logo"
-          width={50}
-          height={50}
-        ></Image>
+        <Link href={"/"}>
+          <Image
+            src="./assets/logo.svg"
+            alt="logo"
+            width={50}
+            height={50}
+          ></Image>
+        </Link>
+
         <div className="flex gap-6 w-[40rem] ">
           <input
             type="search"
@@ -24,13 +28,15 @@ export default function Header() {
           />
         </div>
 
-        <button className="flex items-center bg-white text-xl text-primary p-3 rounded-lg">
+        <button className="flex items-center bg-white text-xl text-primary py-3 px-6 font-medium hover:bg transition-all animation duration-300 rounded-lg">
           Add Question
         </button>
-        <Avatar className="cursor-pointer w-16 h-16 border-2">
-          <AvatarImage src="https://github.com/shadcn.png" />
-          <AvatarFallback>CN</AvatarFallback>
-        </Avatar>
+        <Link href="/profile">
+          <Avatar className="cursor-pointer w-16 h-16 border-2">
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+        </Link>
       </div>
     </header>
   );
