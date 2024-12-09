ccf483b2c42f4b283aec150fdd15c32c7409f70b
Levani Sarishvili <levanisarishvili322@gmail.com>
2024-12-09 10:51:07 +0400

    refactor: update register form layout style

--- src/components/AuthSection.tsx
+++ src/components/AuthSection.tsx
@@ -9,8 +9,8 @@ const AuthSection = ({
   children: React.ReactNode;
 }) => {
   return (
-    <section className="flex justify-center items-center min-h-screen  dark:bg-gray-900 mx-auto px-12 md:px-16">
-      <div className="w-[500px] h-auto p-8 sm:p-16 border border-gray-300 dark:border-gray-700 bg-white dark:bg-gray-800 rounded-lg shadow-lg space-y-10 text-center">
+    <section className="flex w-full justify-center items-center min-h-screen  dark:bg-gray-900 mx-auto px-12 md:px-16">
+      <div className="max-w-[50rem] w-full h-auto p-8 sm:p-16 border border-gray-300 dark:border-gray-700 bg-white dark:bg-gray-800 rounded-lg shadow-lg space-y-10 text-center">
         <h2 className="text-4xl md:text-5xl font-bold text-gray-800 dark:mainText">
           {authType}
         </h2>
